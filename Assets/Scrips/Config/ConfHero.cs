cfcc0c75c1ed46409a621a7dd20d48017ba0915d
hsiuqnav <liuyijia.liu@gmail.com>
2018-08-09 23:27:40 +0800

    example config

---
+++ Assets/Scrips/Config/ConfHero.cs
@@ -0,0 +1,24 @@
+﻿using Kernel.Config;
+using Kernel.Lang.Attribute;
+
+namespace Config
+{
+	[DictionaryConfig(Name = "heroes", ExportPolicy = ConfigExportPolicy.EXPORT_TO_BOTH, LoadAll = true)]
+	public class ConfHero
+	{
+		[Comment("Id")]
+		public int Id;
+
+		[Comment("英雄名")]
+		public string HeroName;
+
+		[Comment("英雄描述")]
+		public string HeroDesc;
+
+		[Comment("英雄血量")]
+		public float Hp;
+
+		[Comment("英雄攻击力")]
+		public float Attack;
+	}
+}
