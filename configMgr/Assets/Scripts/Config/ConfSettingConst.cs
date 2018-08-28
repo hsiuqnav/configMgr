2e470efd9552267a5baa5232548306dd3eef2bc5
hsiuqnav <liuyijia.liu@gmail.com>
2018-08-28 22:19:35 +0800

    const add comment

--- configMgr/Assets/Scripts/Config/ConfSettingConst.cs
+++ configMgr/Assets/Scripts/Config/ConfSettingConst.cs
@@ -1,12 +1,18 @@
 ﻿using Kernel.Config;
+using Kernel.Lang.Attribute;
 
 namespace Config
 {
 	[ConstConfig]
 	public class ConfSettingConst
 	{
+		[Comment("是否使用抗锯齿")]
 		public bool UseAA;
+
+		[Comment("本地化语言")]
 		public string Locale;
+
+		[Comment("版本号")]
 		public string Version;
 
 		public override string ToString()
